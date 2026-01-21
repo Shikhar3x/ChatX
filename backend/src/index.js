88de2499c9034898266036179199c8b51dd358d8
Shikhar <shikharx.dev@gmail.com>
2026-01-22 03:53:19 +0530

    cooked

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,20 @@
 import express from "express";
-import authRoutes from "./routes/authRoutes.js";
-import messageRoute from "./routes/messageRoutes.js";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app, server } from "./utils/socket.js";
 
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
-
-const __dir = path.resolve();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -25,7 +26,7 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoute);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -35,8 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-connectDB().then(() => {
-  server.listen(PORT, () => {
-    console.log("Server is Running in PORT " + PORT);
-  });
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
